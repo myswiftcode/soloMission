3ca6315800791f2777fc18c6d7e985b1744f55e1
sergeev <v.sergeev.m@gmail.com>
2019-07-07 12:50:11 +0700

    Create player, background and sound this project

--- Solo Mission/Solo Mission/GameScene.swift
+++ Solo Mission/Solo Mission/GameScene.swift
@@ -14,27 +14,37 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    let player = SKSpriteNode(imageNamed: "playerShip")
+    let playerBulletSoundEffect = SKAction.playSoundFileNamed("playerBullet.mp3", waitForCompletion: false)
+    
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // Пишем фон
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size = self.size
+        background.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        background.zPosition = 0
+        self.addChild(background)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // Пишем игрока
+        player.setScale(0.5)
+        player.position = CGPoint(x: self.size.width / 2, y: self.size.height * 0.2)
+        player.zPosition = 2
+        self.addChild(player)
+    }
+    
+    func playerCircleBullet(){
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let bullet = SKSpriteNode(imageNamed: "playerBullet")
+        bullet.setScale(0.5)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        let bulletSequence = SKAction.sequence([playerBulletSoundEffect, moveBullet, deleteBullet])
+        bullet.run(bulletSequence)
     }
     
     
@@ -63,15 +73,19 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        playerCircleBullet()
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        // Реализовываем движение игрока от прикосновения на дисплее
+        for touch: AnyObject in touches {
+            
+            let pointOfTouch = touch.location(in: self.view)
+            let previousPointOfTouch = touch.previousLocation(in: self.view)
+            let amountDragged = pointOfTouch.x - previousPointOfTouch.x
+            
+            player.position.x += amountDragged
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
