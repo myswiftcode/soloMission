07545b9a9a08e9b6103e13219af16fc7309f5a9c
sergeev <v.sergeev.m@gmail.com>
2019-07-13 03:23:02 +0700

    Add background music :)

--- Solo Mission/Solo Mission/GameViewController.swift
+++ Solo Mission/Solo Mission/GameViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+// Добавляем AVFoundation(звуковые эффекты)
+import AVFoundation
 
 class GameViewController: UIViewController {
+    
+    var backingAudio = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Загружаем звуковой формат файла
+        let filePath = Bundle.main.path(forResource: "spaceinvaders1", ofType: "mpeg")
+        let audioNSURL = NSURL(fileURLWithPath: filePath!)
+        
+        do { backingAudio = try AVAudioPlayer(contentsOf: audioNSURL as URL) }
+        catch { return print("Cannot Find The Audio!") }
+        
+        // Сколько раз проигрывать файл 1 or 10 или бесконечно -1
+        backingAudio.numberOfLoops = -1
+        // Уровень звука volume = 1 or 10 (в нашем случае зависит от настроек самого девайса play() )
+        backingAudio.play()
         
         let scene = MainMenuScene(size: CGSize(width: 1536, height: 2048))
         
