f49579bb0b424d82b0464ec893ce5e3b11c7c073
sergeev <v.sergeev.m@gmail.com>
2019-07-13 02:39:00 +0700

    Add Main menu in game screen

--- Solo Mission/Solo Mission/GameViewController.swift
+++ Solo Mission/Solo Mission/GameViewController.swift
@@ -15,7 +15,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+        let scene = MainMenuScene(size: CGSize(width: 1536, height: 2048))
         
             let view = self.view as! SKView
             
